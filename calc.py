f5bbd2372374fdf1b888fa64f84101db78d54f08
shriyarathi <42847638+shriyarathi@users.noreply.github.com>
2018-09-06 13:54:53 -0400

    class 02

--- calc.py
+++ calc.py
@@ -1,4 +1,23 @@
-print(42*60+42)
-print(10/1.61)
-print(42.7/6.2)
-print(10/1.61/((42*60+42)/3600))
+import math
+import time
+
+print(math.pi)
+
+radius = 5
+volume = (4.0/3.0) * math.pi * radius**3.0
+
+print ('The volume of a sphere with radius = {rad} is  {vol}'.format(rad=radius, vol=volume))
+
+
+copies = 60
+
+wholesalecost = (24.95*.6* copies)+ (copies*.75) + 3
+
+print ('The wholesale cost for = {cop} is  {who}'.format(cop=copies, who=wholesalecost))
+
+iv = 82
+fv = 89
+
+increase = ((fv-iv)/iv)*100.0
+
+print ('The percentage increase for {} to {} is {}%'.format(iv, fv, increase))
