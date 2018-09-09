61c42982e75b7756eb19d57398a0ffd6d60af045
shriyarathi <42847638+shriyarathi@users.noreply.github.com>
2018-09-09 11:50:14 -0400

    class03

---
+++ session03/class03.py
@@ -0,0 +1,15 @@
+age = int(input('Please enter your age:'))
+
+if age > 21:
+    print ('yes, you can.')
+else:
+    print ('sorry, you cannot')
+
+
+import time
+
+print (time.strftime('%H %M %S'))
+
+print (time.time()/(60*60*24))
+
+print ('\n')
